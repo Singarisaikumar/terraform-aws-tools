fba2e90e1c541e885921cc49454963f814aa19ac
saikumar <wwwsaikumar150@gmail.com>
2025-12-09 16:12:42 +0530

    jenkins

--- cicd/jenkins.sh
+++ cicd/jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo #-secure-protocol+TLSv1
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo -secure-protocol+TLSv1
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-21-openjdk -y
 yum install jenkins -y
